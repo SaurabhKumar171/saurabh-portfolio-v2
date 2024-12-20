74f4d10d1b549aba771e7db3203d29fa2982013f
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2024-12-20 11:39:17 +0530

    Saurabh Portfolio v2

---
+++ src/data/education.ts
@@ -0,0 +1,26 @@
+import { type ExperienceShowcaseListItemProps } from "@/components/experience/experience-showcase-list-item";
+
+export const EDUCATION: ExperienceShowcaseListItemProps[] = [
+  {
+    title: "Bachelor Of Technology in Computer Science",
+    organisation: {
+      name: "Krishna Engineering College",
+      href: "https://eng.rizvi.edu.in/",
+    },
+    date: "2019-2023",
+    location: "Ghaziabad, UP",
+    description:
+      "Relevant courses included Data Structures and Algorithms, Web Design, Cloud Computing, Artificial Neural Network and Database Management.",
+  },
+  {
+    title: "Higher Secondary School",
+    organisation: {
+      name: "Ch. Baldev Singh Model School",
+      href: "https://www.mithibai.ac.in/",
+    },
+    date: "2016-2018",
+    location: "Nagloi, Delhi",
+    description:
+      "Completed the course in Science with 79%. Started freelancing making 3D assets for games. Got interested in game development and learned unity and c#",
+  },
+];
---
+++ src/data/experience.ts
@@ -0,0 +1,36 @@
+import { type ExperienceShowcaseListItemProps } from "@/components/experience/experience-showcase-list-item";
+
+export const EXPERIENCE: ExperienceShowcaseListItemProps[] = [
+  {
+    title: "Software Application Developer",
+    organisation: {
+      name: "Rimes",
+      href: "https://rimes.int/",
+    },
+    date: "Aug 2023 - Present",
+    location: "New Delhi, India",
+    description: "Reactjs and Nextjs app development",
+  },
+  {
+    title: "Software Developer",
+    organisation: {
+      name: "Pheuture Studios Pvt. Ltd.",
+      href: "https://pheuture.com/",
+    },
+    date: "Oct 2022 - June 2023",
+    location: "Noida, India",
+    description:
+      "Learned to work we monorepo and got to learn application architecture, scalling and build tooling to improve CI/CD of micro services.",
+  },
+  {
+    title: "Software Developer Intern",
+    organisation: {
+      name: "Pheuture Studios Pvt. Ltd.",
+      href: "https://pheuture.com/",
+    },
+    date: "July 2022 - Sep 2022",
+    location: "Noida, India",
+    description:
+      "Worked on a team responsible for developing new features and updating old codebases to latest technologies. Learned alot about web development and CI/CD development cycle.",
+  },
+];
